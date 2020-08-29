bfede49fce717594acfff14cffc391887d624df5
in4margaret <in4margaret@gmail.com>
2020-08-29 00:02:37 -0400

    fix: prefix

--- precache-manifest.d5d4a51f197a1c69b509ab00ed8575e0.js
+++ precache-manifest.d5d4a51f197a1c69b509ab00ed8575e0.js
@@ -5,18 +5,18 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
   },
   {
     "revision": "25aa7ede586120b51d9a",
-    "url": "/static/js/2.03cb5702.chunk.js"
+    "url": "/common-ui/static/js/2.03cb5702.chunk.js"
   },
   {
     "revision": "18d938150bc0697ca61bd278149ff911",
-    "url": "/static/js/2.03cb5702.chunk.js.LICENSE.txt"
+    "url": "/common-ui/static/js/2.03cb5702.chunk.js.LICENSE.txt"
   },
   {
     "revision": "61a9781863745973fc7a",
-    "url": "/static/js/main.e09ca4d0.chunk.js"
+    "url": "/common-ui/static/js/main.e09ca4d0.chunk.js"
   },
   {
     "revision": "8851451280c9ab55b7ea",
-    "url": "/static/js/runtime-main.9282a604.js"
+    "url": "/common-ui/static/js/runtime-main.9282a604.js"
   }
 ]);
